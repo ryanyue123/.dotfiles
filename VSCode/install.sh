84d674802531fe4d414f3dd87ade1ef2ca74492f
Ryan Yue <yue.ryan123@gmail.com>
2019-05-09 22:07:04 -0700

    add vscode settings sync gist id

---
+++ VSCode/install.sh
@@ -0,0 +1,3 @@
+# SETTINGS=/Users/ryanyue/Library/Application\ Support/Code/User/
+# ln -sf keybindings.json $SETTINGS/keybindings.json
+# ln -sf settings.json $SETTINGS/settings.json
--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@ sudo -v
 
 declare -a CONFIGS=(
   "macos"
-  "zsh"
   "brew"
+  "zsh"
+  "VSCode"
 )
 
 for config in "${CONFIGS[@]}"
@@ -13,3 +14,5 @@ do
   ./$config/install.sh
 done
 
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ln -sf "$DOTFILES_DIR/.gitconfig" ~
