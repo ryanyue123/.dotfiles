5b8e240c59802e5454c9f1856df74fc89ca613d6
Ryan Yue <yue.ryan123@gmail.com>
2021-03-10 22:08:43 -0800

    update zsh stuffs :)

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,6 +1,12 @@
 # If you come from bash you might have to change your $PATH.
-
 export ZSH=$HOME/.oh-my-zsh
+
+if [ "$(arch)" = "arm64" ]; then
+  eval $(/opt/homebrew/bin/brew shellenv);
+else
+  eval $(/usr/local/bin/brew shellenv);
+fi
+
 
 ZSH_THEME="spaceship"
 SPACESHIP_PROMPT_ORDER=(
@@ -15,8 +21,6 @@ SPACESHIP_TIME_12HR="true"
 SPACESHIP_PROMPT_ADD_NEWLINE="false"
 SPACESHIP_PROMPT_SEPARATE_LINE="false"
 
-plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
-
-source $ZSH/oh-my-zsh.sh
-
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-z)
+source $ZSH/oh-my-zsh.sh 
 export LANG=en_US.UTF-8
--- zsh/install.sh
+++ zsh/install.sh
@@ -1,10 +1,25 @@
+#!/bin/zsh
+
+echo "Installing oh-my-zsh"
+echo $ZSH
+
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+echo "Installing spaceship theme"
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH/custom/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/custom/themes/spaceship.zsh-theme"
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "Installing auto-suggestions plugin"
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH/custom/plugins/zsh-autosuggestions
 
+echo "Installing syntax-highlighting plugin"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/custom/plugins/zsh-syntax-highlighting
+
+echo "Installing zsh-z"
+git clone https://github.com/agkozak/zsh-z $ZSH/custom/plugins/zsh-z
+
+echo "Copying zshrc"
 cp .zshrc ~/.zshrc
 source ~/.zshrc
+
+echo "Done ✅"
